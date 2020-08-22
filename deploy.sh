365b70be8bcced0c4a39835bd8b5d882c9b187b1
周洋 <zhouyang02@qutoutiao.net>
2020-08-22 20:36:42 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -19,6 +19,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:youngchou1997/fe.git master:gh-pages
+# git push -f git@github.com:youngchou1997/fe.git master:gh-pages
+git subtree push --prefix docs/.vuepress/dist origin gh-pages
 
-cd -
+# cd -
